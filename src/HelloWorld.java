80ba6a8c49b5f756a4308a0644ecd4c73240befb
Jayaprakash <jayaaprakashjp@gmail.com>
2017-03-13 07:28:24 +0530

    Hello world demo program

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Japes
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World in Java");
+
+	}
+
+}
